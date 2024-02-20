eff48650fd5156d980e3abe219c992a1846ae52a
jisoo <februstar11@gmail.com>
2024-02-20 22:11:45 +0900

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY = '39efea7389304d74af7037b10a584fb8'
 let news=[]
 const getLatestNews= async()=>{
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`)
+    const url = new URL(`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`)
     console.log("uuu", url)
     const response = await fetch(url)
     const data = await response.json()
